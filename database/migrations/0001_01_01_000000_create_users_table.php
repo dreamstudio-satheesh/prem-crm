d5610a6d3932312fe9117c102aa6f8d2d8b9dce9
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2024-06-13 16:39:55 +0530

    app

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->enum('role', ['Admin', 'Sales', 'Support']);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('role');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
