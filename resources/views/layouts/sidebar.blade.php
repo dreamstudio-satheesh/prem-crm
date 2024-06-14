57d035b44de70fa63c8c07b9cadb044a93f7bceb
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2024-06-14 15:59:13 +0530

    app

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -38,7 +38,7 @@
                             <div class="collapse menu-dropdown" id="sidebarDashboards">
                                 <ul class="nav nav-sm flex-column">
                                     <li class="nav-item">
-                                        <a href="{{ url('home') }}" class="nav-link" data-key="t-analytics">
+                                        <a href="{{ url('home') }}" class="nav-link" data-key="t-dashboards">
                                             Home </a>
                                     </li>
 
@@ -49,25 +49,25 @@
 
 
                         <li class="nav-item">
-                            <a class="nav-link menu-link" href="#sidebarApps" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarApps">
-                                <i class="mdi mdi-view-grid-plus-outline"></i> <span data-key="t-apps">Master</span>
+                            <a class="nav-link menu-link" href="#Master" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="Master">
+                                <i class="mdi mdi-view-grid-plus-outline"></i> <span data-key="t-master">Master</span>
                             </a>
-                            <div class="collapse menu-dropdown" id="sidebarApps">
+                            <div class="collapse menu-dropdown" id="Master">
                                 <ul class="nav nav-sm flex-column">
 
                                 <li class="nav-item">
-                                        <a href="{{ url('user') }}" class="nav-link" data-key="t-calendar">
+                                        <a href="{{ url('user') }}" class="nav-link" data-key="t-master">
                                             Staffs
                                         </a>
                                     </li>
 
                                     <li class="nav-item">
-                                        <a href="{{ url('customer') }}" class="nav-link" data-key="t-calendar">
+                                        <a href="{{ url('customer') }}" class="nav-link" data-key="t-master">
                                             Customer
                                         </a>
                                     </li>
                                     <li class="nav-item">
-                                        <a href="{{ url('contacts') }}" class="nav-link" data-key="t-calendar">
+                                        <a href="{{ url('contacts') }}" class="nav-link" data-key="t-master">
                                              Contacts
                                         </a>
                                     </li>
@@ -81,44 +81,44 @@
                         
                         
                         <li class="nav-item">
-                            <a class="nav-link menu-link" href="#sidebarApps" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarApps">
-                                <i class="mdi mdi-sticker-text-outline"></i> <span data-key="t-apps">Pre Sales</span>
+                            <a class="nav-link menu-link" href="#PreSales" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="PreSales">
+                                <i class="mdi mdi-sticker-text-outline"></i> <span data-key="t-presales">Pre Sales</span>
                             </a>
-                            <div class="collapse menu-dropdown" id="sidebarApps">
+                            <div class="collapse menu-dropdown" id="PreSales">
                                 <ul class="nav nav-sm flex-column">
                                   <li class="nav-item">
-                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-calendar"> Leads
+                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-presales"> Leads
                                         </a>
                                     </li>
                                     
                                     <li class="nav-item">
-                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-calendar"> Opportunities
+                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-presales"> Opportunities
                                         </a>
                                     </li>
                                     <li class="nav-item">
-                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-calendar"> Email Campaign
+                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-presales"> Email Campaign
                                         </a>
                                     </li>
  
 
                                     <li class="nav-item">
-                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-calendar"> Quote
+                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-presales"> Quote
                                         </a>
                                     </li> 
 
                                     <li class="nav-item">
-                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-calendar"> Multiple Quote
+                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-presales"> Multiple Quote
                                         </a>
                                     </li> 
 
                                     <li class="nav-item">
-                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-calendar"> Price Discount & Approval
+                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-presales"> Price Discount & Approval
                                         </a>
                                     </li>
 
 
                                     <li class="nav-item">
-                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-calendar"> Terms & Conditions
+                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-presales"> Terms & Conditions
                                         </a>
                                     </li>
  
@@ -130,29 +130,29 @@
 
 
                         <li class="nav-item">
-                            <a class="nav-link menu-link" href="#sidebarApps" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarApps">
+                            <a class="nav-link menu-link" href="#Sales" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="Sales">
                                 <i class="mdi mdi-sticker-text-outline"></i> <span data-key="t-apps">Sales</span>
                             </a>
-                            <div class="collapse menu-dropdown" id="sidebarApps">
+                            <div class="collapse menu-dropdown" id="Sales">
                                 <ul class="nav nav-sm flex-column"> 
                                 </ul>
                             </div>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link menu-link" href="#sidebarApps" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarApps">
-                                <i class="mdi mdi-sticker-text-outline"></i> <span data-key="t-apps">Services</span>
+                            <a class="nav-link menu-link" href="#Services" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="Services">
+                                <i class="mdi mdi-sticker-text-outline"></i> <span data-key="t-services">Services</span>
                             </a> 
 
-                            <div class="collapse menu-dropdown" id="sidebarApps">
+                            <div class="collapse menu-dropdown" id="Services">
                                 <ul class="nav nav-sm flex-column">  
                                 <li class="nav-item">
-                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-calendar"> AMC  </a>
-                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-calendar"> TDL  </a>
-                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-calendar"> Renewal  </a>
-                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-calendar"> Online Calls  </a>
-                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-calendar"> Onsite Calls  </a>
-                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-calendar"> Calls Closing  </a>
+                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-services"> AMC  </a>
+                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-services"> TDL  </a>
+                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-services"> Renewal  </a>
+                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-services"> Online Calls  </a>
+                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-services"> Onsite Calls  </a>
+                                        <a href="{{ url('reports/hourly') }}" class="nav-link" data-key="t-services"> Calls Closing  </a>
                                     </li>
                                 </ul>
                             </div>
@@ -160,20 +160,20 @@
 
                          
                         <li class="nav-item">
-                            <a class="nav-link menu-link" href="#sidebarApps" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarApps">
+                            <a class="nav-link menu-link" href="#Traning" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="Traning">
                                 <i class="mdi mdi-sticker-text-outline"></i> <span data-key="t-apps">Training</span>
                             </a>
-                            <div class="collapse menu-dropdown" id="sidebarApps">
+                            <div class="collapse menu-dropdown" id="Traning">
                                 <ul class="nav nav-sm flex-column"> 
                                 </ul>
                             </div>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link menu-link" href="#sidebarApps" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarApps">
+                            <a class="nav-link menu-link" href="#UserPermission" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="UserPermission">
                                 <i class="mdi mdi-sticker-text-outline"></i> <span data-key="t-apps">User Permissions  </span>
                             </a>
-                            <div class="collapse menu-dropdown" id="sidebarApps">
+                            <div class="collapse menu-dropdown" id="UserPermission">
                                 <ul class="nav nav-sm flex-column"> 
                                 </ul>
                             </div>
@@ -181,19 +181,19 @@
 
 
                         <li class="nav-item">
-                            <a class="nav-link menu-link" href="#sidebarApps" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarApps">
+                            <a class="nav-link menu-link" href="#Messages" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="Messages">
                                 <i class="mdi mdi-sticker-text-outline"></i> <span data-key="t-apps">Messages</span>
                             </a>
-                            <div class="collapse menu-dropdown" id="sidebarApps">
+                            <div class="collapse menu-dropdown" id="Messages">
                                 <ul class="nav nav-sm flex-column"> 
                                 </ul>
                             </div>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link menu-link" href="#sidebarApps" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarApps">
+                            <a class="nav-link menu-link" href="#Contracts" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="Contracts">
                                 <i class="mdi mdi-sticker-text-outline"></i> <span data-key="t-apps">Contracts</span>
                             </a>
-                            <div class="collapse menu-dropdown" id="sidebarApps">
+                            <div class="collapse menu-dropdown" id="Contracts">
                                 <ul class="nav nav-sm flex-column"> 
                                 </ul>
                             </div>
@@ -212,20 +212,20 @@
 
 
                         <li class="nav-item">
-                            <a class="nav-link menu-link" href="#sidebarApps" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarApps">
+                            <a class="nav-link menu-link" href="#Tickets" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="Tickets">
                                 <i class="mdi mdi-sticker-text-outline"></i> <span data-key="t-apps">Tickets</span>
                             </a>
-                            <div class="collapse menu-dropdown" id="sidebarApps">
+                            <div class="collapse menu-dropdown" id="Tickets">
                                 <ul class="nav nav-sm flex-column"> 
                                 </ul>
                             </div>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link menu-link" href="#sidebarApps" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarApps">
+                            <a class="nav-link menu-link" href="#Reports" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="Reports">
                                 <i class="mdi mdi-sticker-text-outline"></i> <span data-key="t-apps">Reports</span>
                             </a>
-                            <div class="collapse menu-dropdown" id="sidebarApps">
+                            <div class="collapse menu-dropdown" id="Reports">
                                 <ul class="nav nav-sm flex-column">
 
                                     <li class="nav-item">
