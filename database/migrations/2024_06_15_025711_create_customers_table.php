a8c754a9289646546df9105127908fb63790239e
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2024-06-25 08:31:52 +0530

    app

--- database/migrations/2024_06_15_025711_create_customers_table.php
+++ database/migrations/2024_06_15_025711_create_customers_table.php
@@ -6,63 +6,37 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateCustomersTable extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
-       // Schema::dropIfExists('customers');
-     //  php artisan migrate   :fresh
-
-    // php artisan tinker
- //2. Schema::drop('your_table_name')
- //3. php artisan migrate
-
-     // php artisan db:seed 
-    // DB::statement('ALTER TABLE customers CHANGE company_name description bigint(20) ');
- //   php artisan migrate:refresh --path=/database/migrations/fileName.php
-    //php artisan migrate:refresh  --path=/database/migrations/2024_06_15_025711_create_customers_table.php
-    //Schema::dropIfExists('customers');
-    if(Schema::hasTable('customers')) return;
-
         Schema::create('customers', function (Blueprint $table) {
-           
-            $table->id('customer_id'); 
-            $table->string('customer_name')->unique();
-
-            $table->unsignedBigInteger('customertype_id'); //onwer,auditor etc
-           
-            $table->unsignedBigInteger('product_id');//tally silver,gold
-         
+            $table->bigIncrements('customer_id');
+            $table->unsignedInteger('customeraddress_id')->nullable();
+            $table->string('customer_name', 191)->unique();
+            $table->unsignedBigInteger('product_id')->nullable();
             $table->enum('amc', ['yes', 'no'])->default('yes');
             $table->enum('tss_status', ['active', 'inactive'])->default('active');
-            $table->string('tss_adminemail'); 
-            $table->date('tss_expirydate');
-          
-            $table->enum('profile_status', ['Followup', 'Others'])->default('Followup');
-            
-            $table->unsignedBigInteger('staff_id');
-            $table->string('remarks');
-          //  tss,tss_active,tss_admin,tss_expirydate 
-           // (Followup),FollowupExecutive,remarks,
-           
-            $table->string('tally_no')->nullable();
-            $table->string('tally_version')->nullable();
-           
-            $table->boolean('whatsapp_telegram_group')->default(false);
-             
+            $table->string('tss_adminemail', 191)->nullable();
+            $table->date('tss_expirydate')->nullable();
+            $table->enum('profile_status', ['Followup', 'Others'])->nullable();
+            $table->unsignedBigInteger('staff_id')->nullable();
+            $table->string('remarks', 191)->nullable();
+            $table->string('tally_no', 191)->nullable();
+            $table->string('tally_version', 191)->nullable();
+            $table->boolean('whatsapp_telegram_group')->nullable();
             $table->timestamps();
-
-            // Foreign key constraint to contacts table
-         //   $table->foreign('primary_contact_id')->references('contact_id')->on('contacts')->onDelete('cascade');
-
-
-          
-
-
-
         });
-
-       
     }
 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
         Schema::dropIfExists('customers');
