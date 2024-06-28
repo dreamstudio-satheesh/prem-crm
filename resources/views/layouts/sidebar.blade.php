9a63cc543fd3631fb6ed1fbd5f7614dac2a0f5ea
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2024-06-28 15:16:28 +0530

    app

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -16,7 +16,7 @@
                         <img src="{{ assets('') }}/assets/images/logo-sm.png" alt="" height="22">
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ assets('') }}/assets/images/prem-infotech-light.png" alt="" height="17">
+                        <img src="{{ assets('') }}/assets/images/prem-infotech-light.png" alt="" height="34">
                     </span>
                 </a>
                 <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover">
