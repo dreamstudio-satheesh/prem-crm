d394524fbedf5f975f757ed9568ca16723eade7f
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2024-07-21 14:19:41 +0530

    app

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -211,7 +211,7 @@
                             <div class="collapse menu-dropdown" id="Traning">
                                 <ul class="nav nav-sm flex-column">
                                     <li class="nav-item">
-                                        <a href="{{ url('settings') }}" class="nav-link" data-key="t-calendar">  Settings
+                                        <a href="{{ url('settings') }}" class="nav-link" data-key="t-calendar"> General Settings
                                         </a>
                                     </li>
                                     <li class="nav-item">
