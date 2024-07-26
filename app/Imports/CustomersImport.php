00445ee49128d0b362b8883da3ca9ee7db3137f5
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2024-07-27 05:18:31 +0530

    app

--- app/Imports/CustomersImport.php
+++ app/Imports/CustomersImport.php
@@ -6,29 +6,36 @@ use App\Models\Customer;
 use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\ToModel;
 
-class CustomersImport implements ToModel
+class CustomersImport implements ToModel, WithStartRow
 {
-    /**
-     * Maps the incoming row to a model instance.
-     *
-     * @param array $row
-     * @return \Illuminate\Database\Eloquent\Model|null
-     */
+    private $mappings;
+
+    public function __construct(array $mappings)
+    {
+        $this->mappings = $mappings;
+    }
+
+    public function startRow(): int
+    {
+        return 2; // Assuming you want to skip the header
+    }
+
     public function model(array $row)
     {
-        // Check if the essential data is present
-        if (empty($row[1])) {
-            Log::warning('Skipped row due to missing customer name', ['row' => $row]);
-            return null; // Skip this row if customer name is missing
+        $data = [];
+        foreach ($this->mappings as $index => $attribute) {
+            if (isset($row[$index]) && $attribute ) {
+                $data[$attribute] = $row[$index];
+            }
         }
 
-        $customer = new Customer([
-            'customer_name'    => $row[1], // assuming the customer name is in the second column
-            'tally_serial_no'  => $row[5] ?? null, // assuming the tally serial number is in the sixth column, use null if not present
-        ]);
+        $customer = new Customer($data);
 
-        $customer->save(); // Save the customer
-        Log::info('Successfully imported customer', ['customer_id' => $customer->customer_id]);
+        if ($customer->save()) {
+            Log::info('Successfully imported customer', ['customer_id' => $customer->customer_id]);
+        } else {
+            Log::error('Failed to save customer', ['data' => $customer->attributesToArray()]);
+        }
 
         return $customer;
     }
