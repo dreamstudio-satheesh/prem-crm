e87832d2897c25494d6f4a9a88ff87dd39709062
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2024-07-27 05:37:40 +0530

    app

--- app/Imports/CustomersImport.php
+++ app/Imports/CustomersImport.php
@@ -3,7 +3,6 @@
 namespace App\Imports;
 
 use App\Models\Customer;
-use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithStartRow;
 
@@ -18,26 +17,30 @@ class CustomersImport implements ToModel, WithStartRow
 
     public function startRow(): int
     {
-        return 2; // Assuming you want to skip the header
+        return 2; // Start processing from the second row to skip the header
     }
 
     public function model(array $row)
     {
         $data = [];
         foreach ($this->mappings as $index => $attribute) {
-            if (isset($row[$index]) && $attribute ) {
+            if (isset($row[$index]) && $attribute !== null && $row[$index] !== '') {
                 $data[$attribute] = $row[$index];
             }
         }
 
-        $customer = new Customer($data);
-
-        if ($customer->save()) {
-            Log::info('Successfully imported customer', ['customer_id' => $customer->customer_id]);
-        } else {
-            Log::error('Failed to save customer', ['data' => $customer->attributesToArray()]);
+        // Check if both required attributes are available before saving
+        if (!empty($data['customer_name']) && !empty($data['tally_serial_no'])) {
+            // Update or create customer based on a unique identifier, such as 'tally_serial_no'
+            $customer = Customer::updateOrCreate(
+                ['tally_serial_no' => $data['tally_serial_no']], // Unique identifier
+                $data // Values to update or create with
+            );
+            
+            return $customer;
         }
 
-        return $customer;
+        // Optionally, return null or some indication of skipped row due to missing required fields
+        return null;
     }
 }
