a4b0bbff0ec1e0accfb8c7783f6aac6edbb2d7f9
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2024-07-28 08:27:23 +0530

    app

--- resources/views/customer_import_preview.blade.php
+++ resources/views/customer_import_preview.blade.php
@@ -1,11 +1,7 @@
-<!-- resources/views/customer_import_preview.blade.php -->
-
-
 @extends('layouts.admin')
 
 @section('content')
 
-<!-- resources/views/customer_import_preview.blade.php -->
 <div class="card">
     <div class="card-header align-items-center d-flex">
         <div class="col">
@@ -16,7 +12,7 @@
         </div>
     </div>
     <div class="card-body">
-        <form action="{{ route('customer_import.import') }}" method="POST">
+        <form id="importForm" action="{{ route('customer_import.import') }}" method="POST">
             @csrf
             <input type="hidden" name="tempFilePath" value="{{ $tempFilePath }}">
             <div class="table-responsive mt-5">
@@ -26,7 +22,7 @@
                             @foreach($headers as $index)
                             <th style="min-width: 150px;">
                                 {{ $rawHeaders[$index] }}
-                                <select name="mappings[{{ $index }}]" class="form-select form-select-sm">
+                                <select name="mappings[{{ $index }}]" class="form-select form-select-sm import-select">
                                     <option value="">Select Field</option>
                                     @foreach($columnOptions as $field => $label)
                                     <option value="{{ $field }}">{{ $label }}</option>
@@ -53,39 +49,37 @@
     </div>
 </div>
 
-
 @endsection
-
 
 @push('scripts')
     <script>
     document.getElementById('importForm').addEventListener('submit', function(event) {
-    let selects = document.querySelectorAll('.import-select');
-    let selectedFields = {};
-    let tallySerialSelected = false;
-    let duplicateSelection = false;
+        let selects = document.querySelectorAll('.import-select');
+        let selectedFields = {};
+        let tallySerialSelected = false;
+        let duplicateSelection = false;
 
-    selects.forEach(function(select) {
-        let value = select.value;
-        if (value) {
-            if (value === 'tally_serial_no') {
-                tallySerialSelected = true;
+        selects.forEach(function(select) {
+            let value = select.value;
+            if (value) {
+                if (value === 'tally_serial_no') {
+                    tallySerialSelected = true;
+                }
+                if (selectedFields[value]) {
+                    duplicateSelection = true;
+                } else {
+                    selectedFields[value] = true;
+                }
             }
-            if (selectedFields[value]) {
-                duplicateSelection = true;
-            } else {
-                selectedFields[value] = true;
-            }
+        });
+
+        if (!tallySerialSelected) {
+            alert('Please select the Tally Serial No field.');
+            event.preventDefault();
+        } else if (duplicateSelection) {
+            alert('Each field can only be selected once.');
+            event.preventDefault();
         }
-    });
-
-    if (!tallySerialSelected) {
-        alert('Please select the Tally Serial No field.');
-        event.preventDefault();
-    } else if (duplicateSelection) {
-        alert('Each field can only be selected once.');
-        event.preventDefault();
-    }
     });
     </script>
 @endpush
