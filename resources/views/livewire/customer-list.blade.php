7e93583a7a4d4fe82e5afe7235fcf379a2c1607c
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2024-07-29 23:04:14 +0530

    app

--- resources/views/livewire/customer-list.blade.php
+++ resources/views/livewire/customer-list.blade.php
@@ -135,7 +135,7 @@
                                 <tr>
                                     <td>{{ $customers->firstItem() + $index }}</td> <!-- Serial number -->
                                     <td style="max-width: 150px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">
-                                        @if(strlen($customer->customer_name) > 30)
+                                        @if(strlen($customer->customer_name) > 25)
                                         <marquee behavior="scroll" direction="left">{{ $customer->customer_name }}</marquee>
                                         @else
                                         {{ $customer->customer_name }}
