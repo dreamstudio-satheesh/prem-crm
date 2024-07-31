bc6cdb5df02b4f25c4e0e82cc65fbe7fa604e50d
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2024-07-31 21:11:04 +0530

    app

--- app/Mail/CallClosedNotification.php
+++ app/Mail/CallClosedNotification.php
@@ -42,32 +42,7 @@ class CallClosedNotification extends Mailable
      */
     protected function updateMailConfig()
     {
-         $settings = Setting::whereIn('key', [
-            'email.mail_host',
-            'email.mail_port',
-            'email.mail_encryption',
-            'email.mail_username',
-            'email.mail_password',
-            'email.from_address',
-            'email.from_name'
-        ])->pluck('value', 'key');
-
-        $config = [
-            'driver'     => 'smtp',
-            'host'       => $settings['email.mail_host'],
-            'port'       => $settings['email.mail_port'],
-            'encryption' => $settings['email.mail_encryption'],
-            'username'   => $settings['email.mail_username'],
-            'password'   => $settings['email.mail_password'],
-            'from'       => [
-                'address' => $settings['email.from_address'],
-                'name'    => $settings['email.from_name']
-            ],
-        ];
-
-        config(['mail.mailers.smtp' => $config]);
-        config(['mail.from.address' => $settings['email.from_address']]);
-        config(['mail.from.name' => $settings['email.from_name']]);
+       
 
     }
 }
