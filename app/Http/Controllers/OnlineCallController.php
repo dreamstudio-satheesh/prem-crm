bb0d5fa1c723eca6f671ed9e22561ac66db37615
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2024-07-31 21:29:29 +0530

    app

--- app/Http/Controllers/OnlineCallController.php
+++ app/Http/Controllers/OnlineCallController.php
@@ -130,6 +130,8 @@ class OnlineCallController extends Controller
             $customer = Customer::find($request->customer_id);
             $emails = $customer->addressBooks->pluck('email')->filter();
 
+            return  $emails;
+
             // Log email sending process
             foreach ($emails as $email) {
                 Log::info('Sending email to: ' . $email);
